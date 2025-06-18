59467095da808b94460fcf1f3ad3583f10b23a84
chungtamz1801 <chungtam1801@gmail.com>
2025-06-18 15:58:16 +0700

    Disable Connection Button

--- MessageScreen.cs
+++ MessageScreen.cs
@@ -17,7 +17,7 @@ namespace ChatApp
     public partial class MessageScreen : Form
     {
         Socket socket;
-        EndPoint epRemote,epLocal;
+        EndPoint epRemote,epLocal,epTemp;
         byte[] buffer;
         byte[] keyAES;
         long[] publicKeyRSA = new long[2];
@@ -37,28 +37,21 @@ namespace ChatApp
             //set up socket
             socket = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
             socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
-            //gen key for RSA
-            /*long n, en, de;
-            RSAT.GenerateKeys(out n,out en,out de);
-            message_lst.Items.Add("System:" + en);
-            message_lst.Items.Add("System:" + de);*/
             //get user IP
             Information_IP.Text = ip;
             Information_Port.Text = port.ToString();
-            Friend_IP.Text = Default.GetLocalIP();           
+            Friend_IP.Text = Default.GetLocalIP();
+            epLocal = new IPEndPoint(IPAddress.Parse(ip), port);
+            socket.Bind(epLocal);
+            epTemp =new IPEndPoint(IPAddress.Any, 0);
+            buffer = new byte[16 * storage + 1];
+            socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epTemp, new AsyncCallback(MessageCallBack), buffer);
         }
 
         private void connect_btn_Click(object sender, EventArgs e)
         {
-            ///binding socket
-            epLocal = new IPEndPoint(IPAddress.Parse(ip), port);
-            socket.Bind(epLocal);
             //connect to remote IP  
-            epRemote = new IPEndPoint(IPAddress.Parse(Friend_IP.Text), Convert.ToInt32(Friend_Port.Text));
-            socket.Connect(epRemote);
-            //Listening the specific port
-            buffer = new byte[16*storage+1];
-            socket.BeginReceiveFrom(buffer, 0, buffer.Length, SocketFlags.None, ref epRemote, new AsyncCallback(MessageCallBack), buffer);
+            //epRemote = new IPEndPoint(IPAddress.Parse(Friend_IP.Text), Convert.ToInt32(Friend_Port.Text));
         }
 
         private void send_btn_Click(object sender, EventArgs e)
@@ -70,7 +63,7 @@ namespace ChatApp
                 Array.Copy(AES.Encrypt(message_tbx.Text, keyAES, new byte[16]), 0, sendingMessage, 1, 16);
                 //send message
                 sendingMessage[0] = 0;
-                socket.Send(sendingMessage);
+                socket.SendTo(sendingMessage,epRemote);
 
                 message_lst.Items.Add("Me: " + message_tbx.Text);
 
@@ -84,19 +77,22 @@ namespace ChatApp
 
         private void key_btn_Click(object sender, EventArgs e)
         {
-            long  n, en, de;
+            epRemote = new IPEndPoint(IPAddress.Parse(Friend_IP.Text), Convert.ToInt32(Friend_Port.Text));
+            long n, en, de;
             RSAT.GenerateKeys(out n, out en, out de);
             publicKeyRSA = new long[]{ en, n };
             privateKeyRSA = new long[] { de, n };
             message_lst.Items.Add($"System: Private Key là ({de},{n})");
             message_lst.Items.Add($"System: Public Key là ({en},{n})");
             string data = Default.TrasferLongArrayToString(publicKeyRSA);
+            IPEndPoint ep = epLocal as IPEndPoint; 
+            data = data + "/" + ep.ToString();
             ASCIIEncoding aEncoding = new ASCIIEncoding();
             byte[] temp = aEncoding.GetBytes(data);
             byte[] sendingData = new byte[temp.Length+1];
             Array.Copy(temp,0, sendingData,1,temp.Length);
             sendingData[0] = 1;
-            socket.Send(sendingData);
+            socket.SendTo(sendingData,epRemote);
         }
 
         private void MessageCallBack(IAsyncResult aResult)
@@ -118,6 +114,8 @@ namespace ChatApp
                     Array.Copy(receivedData, 1, bytes, 0, 16*storage);
                     ASCIIEncoding aEncoding = new ASCIIEncoding();
                     string temp = aEncoding.GetString(bytes);
+                    string[] list = temp.Split('/');
+                    temp = list[0];
                     long[] pubKey = Default.TrasferStringToLongArray(temp);
                     message_lst.Items.Add($"System: Public Key nhận được là ({pubKey[0]},{pubKey[1]})");
                     //Gen key AES
@@ -133,8 +131,21 @@ namespace ChatApp
                     byte[] data = aEncoding.GetBytes(sendingMessage);
                     byte[] sendingData = new byte[data.Length+1];
                     Array.Copy(data, 0, sendingData, 1, data.Length);
-                    sendingData[0] = 2;
-                    socket.Send(sendingData);
+                    string s = list[1] + " want to connect!";
+                    DialogResult dialogResult = MessageBox.Show(s, "Request", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        sendingData[0] = 2;
+                        epRemote = new IPEndPoint(IPAddress.Parse(list[1].Split(':')[0]), Convert.ToInt32(list[1].Split(':')[1]));
+                        socket.SendTo(sendingData, epRemote);
+                    }
+                    else if(dialogResult == DialogResult.No)
+                    {
+                        sendingData = new byte[] {3};
+                        epRemote = new IPEndPoint(IPAddress.Parse(list[1].Split(':')[0]), Convert.ToInt32(list[1].Split(':')[1]));
+                        socket.SendTo(sendingData, epRemote);
+                    }
+                    
                 }
                 else if(receivedData[0] == 2)
                 {
@@ -147,6 +158,10 @@ namespace ChatApp
                     keyAES = RSAT.Decrypt(temp, privateKeyRSA[0], privateKeyRSA[1]);
                     message_lst.Items.Add($"System: Key AES sau khi giải mã là {BitConverter.ToString(keyAES)}");
                     //
+                }
+                else if (receivedData[0] == 3)
+                {
+                    message_lst.Items.Add($"System: Request was denied");
                 }
 
                 //Convert byte[] to string
